eac6f7ec1ae2a44c4f6fc1540833b6d8e60f8398
nam9nine <southgyuios19@gmail.com>
2024-06-03 10:08:16 +0900

    [DES] - 3. bit 확장 32bit->48bit

--- des/ex1.c
+++ des/ex1.c
@@ -4,7 +4,10 @@ typedef unsigned char BYTE;
 typedef unsigned int UINT;
 
 void IP(BYTE *in, BYTE *out);
-BYTE ip[] = {
+void BtoW(BYTE *in, UINT *l, UINT *r);
+void EP(UINT *r, BYTE *out);
+
+BYTE ip[64] = {
     58, 50, 42, 34, 26, 18, 10, 2,
     60, 52, 44, 36, 28, 20, 12, 4,
     62, 54, 46, 38, 30, 22, 14, 6,
@@ -13,6 +16,16 @@ BYTE ip[] = {
     59, 51, 43, 35, 27, 19, 11, 3,
     61, 53, 45, 37, 29, 21, 13, 5,
     63, 55, 47, 39, 31, 23, 15, 7};
+
+BYTE E[48] = {
+    32, 1, 2, 3, 4, 5,
+    4, 5, 6, 7, 8, 9,
+    8, 9, 10, 11, 12, 13,
+    12, 13, 14, 15, 16, 17,
+    16, 17, 18, 19, 20, 21,
+    20, 21, 22, 23, 24, 25,
+    24, 25, 26, 27, 28, 29,
+    28, 29, 30, 31, 32, 1};
 
 int main()
 {
@@ -62,7 +75,21 @@ void BtoW(BYTE *in, UINT *l, UINT *r)
         }
         else
         {
-            *r |= (UINT)in[i] << (56 - (i * 8)); // 오른쪽 32비트로 변환
+            *r |= (UINT)in[i] << (56 - (i * 8));
+        }
+    }
+}
+
+void EP(UINT *r, BYTE *out)
+{
+    int i;
+    UINT mask = 0x80000000;
+
+    for (i = 0; i < 48; i++)
+    {
+        if (*r & (mask >> (E[i] - 1)))
+        {
+            out[i / 8] |= (BYTE)(0x80 >> (i % 8));
         }
     }
 }
